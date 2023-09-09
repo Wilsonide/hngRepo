26c97be03899bbc794f3124714e037c5dce9ecdb
Wilson Icheku <ichekuwilson538@gmail.com>
2023-09-09 03:13:05 +0100

    refactor utc format

--- backend/api/index.js
+++ backend/api/index.js
@@ -8,7 +8,7 @@ app.get("/api/", (req, res) => {
     res.json({
         "slack_name": req.query.slack_name,
         "current_day": new Intl.DateTimeFormat('en-us',{weekday:"long"}).format(new Date()),
-        "utc_time": new Date().toISOString(),
+        "utc_time": new Date().toISOString().slice(0,19) + "Z",
         "track" : req.query.track,
         "github_file_url": "https://github.com/Wilsonide/hngRepo/blob/master/backend/api/index.js",
         "github_repo_url": "https://github.com/Wilsonide/hngRepo",
