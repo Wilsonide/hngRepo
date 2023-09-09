9d6d37c35509314508988fba02c7e4bf1afa8b8a
Wilson Icheku <ichekuwilson538@gmail.com>
2023-09-09 08:18:52 +0100

    undo

--- firstTask/index.js
+++ firstTask/index.js
@@ -4,7 +4,5 @@ const date = new Date()
 /* const utcTime = date.UTC(); */
 const dayFormat = new Intl.DateTimeFormat('en-US',{weekday:'long'})
 /* console.log("utc in milliseconds", utcTime); */
-const me = dayFormat.format(date);
-console.log(me);
-pday.textContent= me;
+pday.textContent=  dayFormat.format(date);
 myTime.textContent = Date.now();
