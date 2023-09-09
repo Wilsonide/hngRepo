f14a29fedc5aee45ca31bf4a3be47411ae927bdc
Wilson Icheku <ichekuwilson538@gmail.com>
2023-09-09 15:46:34 +0100

    done

--- firstTask/index.js
+++ firstTask/index.js
@@ -1,6 +1,6 @@
-const day = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+const day = document.getElementById("day);
 
- const time = document.querySelector('[data-testid="currentUTCTime"]');
+ const time = document.getElementById("time);
 
 const timeNow = `${new Date().getUTCHours()}:${new Date().getUTCMinutes()}:${new Date().getSeconds()}` 
 
