594c29a26e3343969290c0653561435ea38c16a3
Wilson Icheku <ichekuwilson538@gmail.com>
2023-09-10 16:19:25 +0100

    hot fix

--- firstTask/index.js
+++ firstTask/index.js
@@ -4,6 +4,5 @@ const day = document.querySelector('[data-testid="currentDayOfTheWeek"]');
 
 const timeNow = setInterval(Date.now(),1000);
 time.innerHTML = timeNow; 
-setInterval(timeNow,1000);
 
 day.innerHTML = new Date().toLocaleString('en-US', {weekday: 'long'});
