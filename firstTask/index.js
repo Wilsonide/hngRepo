fcf9fc726d4ae8144770d4abe2e8b5fdd055da14
Wilson Icheku <ichekuwilson538@gmail.com>
2023-09-10 16:18:26 +0100

    hot fix

--- firstTask/index.js
+++ firstTask/index.js
@@ -2,7 +2,7 @@ const day = document.querySelector('[data-testid="currentDayOfTheWeek"]');
 
  const time = document.querySelector('[data-testid="currentUTCTime"]');
 
-const timeNow = Date.now();
+const timeNow = setInterval(Date.now(),1000);
 time.innerHTML = timeNow; 
 setInterval(timeNow,1000);
 
